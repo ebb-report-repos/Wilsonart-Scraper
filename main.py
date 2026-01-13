cab2378f83d58561851d44ccc66cea054f605a91
ebb-report-repos <ebbradley.github@gmail.com>
2026-01-13 10:52:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
 import time
 import requests
 #============================
